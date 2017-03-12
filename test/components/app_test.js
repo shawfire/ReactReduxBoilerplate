f6f1228038fa3e56ef9a28b3af3a3699574d1bee
John Shaw <shaw_fire@yahoo.com>
2017-03-12 17:57:46 +1100

    Add test to check that CommentBox is added to parent App component.

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,23 +3,10 @@ import App from '../../src/components/app';
 
 // Use describe to group together similar tests.
 describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  it('show a comment box', () => {
+    const component = renderComponent(App);
+    expect(component.find('.comment-box')).to.exist;
   });
 
-  it('expect component to exist', () => {
-    // Use 'expect' to make an 'assertion' about a target
-    // const component = renderComponent(App);
-    expect(component).to.exist;
-  });
-
-  // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-
-    // Use 'expect' to make an 'assertion' about a target
-    expect(component).to.exist;
-    expect(component).to.contain('This is a React, Redux Boiler Plate App...');
-  });
 });
