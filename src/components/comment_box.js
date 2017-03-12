867f4e3bff4f21ab23dd94c9bb79d6795c48efdd
John Shaw <shaw_fire@yahoo.com>
2017-03-12 17:25:20 +1100

    CommentText components pass.

---
+++ src/components/comment_box.js
@@ -0,0 +1,26 @@
+// Note: Cmd Alt i - Opens up console window in Chrome
+//  Select console - JS tab - to see errors
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new compoent.
+//  This component should produce some HTML.
+export default class CommentBox extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //   };
+  // }
+
+  // // JSX - What looks like HTML in our JavaScript
+  // //  this code get s transpiled with babel to produce JS
+  // //  i.e. babeljs.io/repl
+  render() {
+    return (
+      <div>
+        <textarea />
+        <button></button>
+      </div>
+    );
+  }
+}
